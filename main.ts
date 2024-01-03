d26b853c23f5494d213effc93560b03d9dd72d6c
Vani Indrali <vanisindrali@gmail.com>
2024-01-03 18:02:03 +0530

    Added Light and Material to the Object

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
 const scene = new THREE.Scene()
 
@@ -10,43 +11,52 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 30
+camera.position.z = 35
 
 //adding axes helper to the object
 scene.add(new THREE.AxesHelper(15)); 
+
+// adding light for the meshphongmaterial
+const frontLight = new THREE.PointLight(0xffffff, 1000)
+frontLight.position.set(2.5, -7.5, 15)
+scene.add(frontLight)
+
+const backLight = new THREE.PointLight(0xffffff, 1000)
+backLight.position.set(2.5, -7.5, -15)
+scene.add(backLight)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setClearColor(0xffffff)
 document.body.appendChild(renderer.domElement)
 
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true
-})
-
 // for moving the objects
 const orbitControl = new OrbitControls(camera,renderer.domElement);
 
-// loading chessboard object using objectloader
-const chessBoard = new OBJLoader();
-chessBoard.load(
-    './Model/chessboardModel.obj',
-    (object) => {
-        // console.log(object);
-        // console.log(object.children);
-        object.traverse(function (child) {
-            if ((child as THREE.Mesh).isMesh) {
-                (child as THREE.Mesh).material = material
-            }
-        })
-        scene.add(object);
+// add materials to the object using MTLLoader & load object using OBJLoader
+const chessBoardMaterial = new MTLLoader();
+chessBoardMaterial.load(
+    './Model/chessboardModel.mtl',
+    (materials) => {
+        materials.preload()
+
+        // loading chessboard object using objectloader
+        const chessBoard = new OBJLoader();
+
+        // by default it is MeshPhongMaterial hence light is needed
+        chessBoard.setMaterials(materials)
+        chessBoard.load(
+        './Model/chessboardModel.obj',
+        (object) => {
+            scene.add(object);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
     },
     (error) => {
         console.log(error)
+    }
+)
     }
 )
 
